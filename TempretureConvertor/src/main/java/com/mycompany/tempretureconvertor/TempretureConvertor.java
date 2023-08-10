8e8b728da4729067d00fe20bf902521497e6b11f
Siddhant Chauhan <72564955+siddhant8523@users.noreply.github.com>
2023-08-10 16:46:07 +0530

    Add files via upload

---
+++ TempretureConvertor/src/main/java/com/mycompany/tempretureconvertor/TempretureConvertor.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tempretureconvertor;
+
+ class TempretureConvertor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
